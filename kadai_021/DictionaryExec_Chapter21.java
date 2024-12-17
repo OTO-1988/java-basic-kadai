d9c824b3e7541cbdeea3c8cc1ad4fe5073cd4952
OTO-1988 <t.ogura.gk@gmail.com>
2024-12-18 01:21:55 +0900

    kadai_021を追加

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,14 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		Dictionary_Chapter21 dic = new Dictionary_Chapter21();
+		
+//		検索する語句の配列
+		String[] searchWords = {"apple","banana","grape","orange"};
+		
+//		検索メソッドの呼び出し
+		dic.SearchWords(searchWords);
+		}
+	}
